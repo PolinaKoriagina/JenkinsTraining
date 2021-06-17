bdd83904159d40c7d6e052f752ce0742c1ec7bae
PolinaKoriagina <koriagina.p@gmail.com>
2021-06-17 12:46:26 +0200

    added close driver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
@@ -10,4 +13,17 @@ public class TestBase {
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
+
+    @AfterEach
+    public void tearDown() {
+//        String sessionId = getSessionId();
+//
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+        closeWebDriver();
+
+    //    Attach.addVideo(sessionId);
+    }
+
 }
