0e4c5efa8ee41415248a2f920023fefe1511d315
PolinaKoriagina <koriagina.p@gmail.com>
2021-06-17 14:28:05 +0200

    added allure video attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,29 +6,47 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class TestBase {
     @BeforeAll
     static void setUp() {
-        // choose the browser and size
+        // choose size of browser, added Allure, added screenshot,
+        // page sources, browser console logs, enabled Video,
+        // added remote test running with Selenoid
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
+    //        String sessionId = getSessionId();
+    public static String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
+
     @AfterEach
     public void tearDown() {
+        String sessionId = getSessionId();
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-//        String sessionId = getSessionId();
-
 
         closeWebDriver();
 
         //    Attach.addVideo(sessionId);
+        Attach.addVideo(sessionId);
     }
 
 }
+
